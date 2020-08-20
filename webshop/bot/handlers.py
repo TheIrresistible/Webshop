36ff2d2bfaa0f1b58324051303a9c195e45833ca
TheIrresistible <62150688+TheIrresistible@users.noreply.github.com>
2020-08-20 12:47:26 +0300

    wgsg

--- webshop/bot/handlers.py
+++ webshop/bot/handlers.py
@@ -105,7 +105,7 @@ def show_cart(message):
     kb.add(button)
     for u in User.objects(user_id=message.from_user.id):
         for c in Cart.objects(customer=u):
-            list_of_products.append(c.products)
+            list_of_products.append(*c.products)
             print(list_of_products)
 
     for product in list_of_products:
